27133e1e7cb1a2c15f7a464c6b3aacf68571b165
poetrainy <manamisato2001@gmail.com>
2025-01-14 07:18:38 +0900

    refactor: 選択した楽曲を常に先頭に配置してシャッフルする

--- frontend/app/libraries/shuffle.ts
+++ frontend/app/libraries/shuffle.ts
@@ -1,11 +1,19 @@
 import type { PlaylistType } from "~/types/common";
 
-export const shuffle = (playlist: PlaylistType[]) => {
-  let newPlaylist = [...playlist];
+export const shuffle: (
+  playlist: PlaylistType[],
+  currentIndex?: number
+) => PlaylistType[] = (playlist: PlaylistType[], currentIndex = 0) => {
+  const currentTrack = playlist[currentIndex];
+  const newPlaylist = [
+    ...playlist.slice(0, currentIndex),
+    ...playlist.slice(currentIndex + 1),
+  ];
+
   for (let i = newPlaylist.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [newPlaylist[i], newPlaylist[j]] = [newPlaylist[j], newPlaylist[i]];
   }
 
-  return newPlaylist;
+  return [currentTrack, ...newPlaylist];
 };
