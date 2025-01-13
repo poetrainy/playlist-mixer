755a1b5df53411e55781b21e695c4587650d828d
poetrainy <manamisato2001@gmail.com>
2025-01-14 07:18:07 +0900

    fix: Updated appearance

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -8,10 +8,32 @@ import { cva } from "class-variance-authority";
 import { DUMMY_SPOTIFY_TRACKS, DUMMY_YOUTUBE_TRACKS } from "~/constants/common";
 import { shuffle } from "~/libraries/shuffle";
 
-const playingIconMap = {
+const currentIconMap = {
   youtube: ICON_YOUTUBE,
   spotify: ICON_SPOTIFY,
 } as const;
+
+const cvaTrack = cva(
+  [
+    "flex",
+    "justify-start",
+    "gap-2",
+    "w-full",
+    "p-1",
+    "rounded",
+    "outline-offset-4",
+
+    "focus-visible:outline-teal-600",
+  ],
+  {
+    variants: {
+      current: {
+        false: ["hover:bg-gray-200", "active:bg-gray-300"],
+        true: ["bg-teal-500"],
+      },
+    },
+  }
+);
 
 const cvaTrackTitle = cva(
   [
@@ -20,25 +42,29 @@ const cvaTrackTitle = cva(
     "whitespace-nowrap",
     "overflow-hidden",
     "text-ellipsis",
+    "text-left",
   ],
   {
     variants: {
-      playing: {
+      current: {
         false: ["text-gray-900"],
-        true: ["text-teal-600"],
+        true: ["text-white"],
       },
     },
   }
 );
 
-const cvaTrackArtistName = cva(["text-gray-500", "text-[0.625rem]"], {
-  variants: {
-    playing: {
-      false: [],
-      true: ["font-bold"],
+const cvaTrackArtistName = cva(
+  ["text-gray-500", "text-[0.625rem]", "text-left"],
+  {
+    variants: {
+      current: {
+        false: [],
+        true: ["text-white", "opacity-80", "font-bold"],
+      },
     },
-  },
-});
+  }
+);
 
 const cvaTrackThumbnail = cva(
   [
@@ -52,7 +78,7 @@ const cvaTrackThumbnail = cva(
   ],
   {
     variants: {
-      playing: {
+      current: {
         false: [],
         true: [],
       },
@@ -60,7 +86,7 @@ const cvaTrackThumbnail = cva(
   }
 );
 
-const cvaPlaying = cva(
+const cvaCurrentType = cva(
   [
     "flex",
     "justify-center",
@@ -102,7 +128,7 @@ export default function Home() {
     ...youtubePlaylist.map((track) => ({ type: "youtube" as const, track })),
     ...spotifyPlaylist.map((track) => ({ type: "spotify" as const, track })),
   ]);
-  const playing = playlist[0];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const getYoutube = async () => {
     setError(false);
@@ -129,126 +155,152 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-stretch gap-6 w-[90vw] max-w-[50rem] m-auto py-8">
-      <div className="flex flex-col gap-2 text-center">
-        <h1 className="text-teal-800 text-3xl font-bold">
-          Music player by YouTube, Spotify
-        </h1>
-        <p className="text-gray-600 text-sm">
-          YouTubeとSpotifyのプレイリストをシャッフルして再生します。
-        </p>
-      </div>
-      <div className="flex flex-col gap-3">
-        <label className="flex flex-col gap-1 w-full">
-          <span className="text-teal-600 text-sm font-bold">YouTube</span>
-          <div className="relative">
-            <span className="flex justify-center items-center size-11 text-white bg-[#DA1725] absolute top-[2px] left-[2px] rounded-[4px_0_0_4px]">
-              <img src={ICON_YOUTUBE} className="size-6" />
-            </span>
-            <input
-              name="youtube"
-              className="w-full h-12 text-gray-900 bg-white border-2 border-gray-300 pl-14 pr-3 rounded-md outline-offset-4 focus-visible:outline-teal-600"
-              placeholder="https://youtube.com/playlist?list=xxx"
-              value={youtubeUrl}
-              onChange={(event) => setYoutubeUrl(event.target.value)}
-            />
-          </div>
-        </label>
-        <label className="flex flex-col gap-1 w-full">
-          <span className="text-teal-600 text-sm font-bold">Spotify</span>
-          <div className="relative">
-            <span className="flex justify-center items-center size-11 bg-[#2ebd59] absolute top-[2px] left-[2px] rounded-[4px_0_0_4px]">
-              <img src={ICON_SPOTIFY} className="size-6" />
-            </span>
-            <input
-              name="spotify"
-              className="w-full h-12 text-gray-900 bg-white border-2 border-gray-300 pl-14 pr-3 rounded-md outline-offset-4 focus-visible:outline-teal-600"
-              placeholder="https://open.spotify.com/playlist/xxx"
-              value={spotifyUrl}
-              onChange={(event) => setSpotifyUrl(event.target.value)}
-            />
-          </div>
-        </label>
-      </div>
-      <button
-        className="flex-none w-20 h-12 text-white px-3 rounded-md font-bold outline-offset-[3px] transition-colors enabled:bg-teal-500 enabled:hover:bg-teal-600 enabled:active:bg-teal-700 focus-visible:outline-teal-600 disabled:bg-gray-300"
-        disabled={!youtubeUrl.length || !spotifyUrl.length}
-        onClick={() => getYoutube()}
-      >
-        検索
-      </button>
-      {error && (
-        <p>
-          プレイリストがヒットしませんでした。URLを確認し、もう一度お試しください。
-        </p>
-      )}
-      <div className="flex flex-col items-start gap-5">
-        <div className="flex justify-center items-center size-full max-h-96 max-w-96 aspect-square overflow-hidden relative rounded-2xl shadow-2xl">
-          {playing.type === "youtube" ? (
-            <>
-              <div className="flex justify-center items-center h-full aspect-video overflow-hidden absolute">
-                <div className="flex justify-center items-center aspect-video blur-md scale-105">
-                  <img src={playing.track.thumbnail.url} />
-                </div>
-              </div>
-              <div className="flex items-center aspect-video overflow-hidden relative z-auto">
-                <img src={playing.track.thumbnail.url} />
-              </div>
-            </>
-          ) : (
-            <img src={playing.track.thumbnail.url} />
-          )}
-        </div>
-        <div className="flex flex-col items-stretch gap-3">
-          <div className="text-gray-900 text-2xl font-bold leading-9">
-            {playing.track.title}
-          </div>
-          <div className="flex items-center gap-1.5 text-gray-700">
-            <span className={cvaPlaying({ type: playing.type })}>
-              <img
-                src={playingIconMap[playing.type ?? "youtube"]}
-                className="size-5"
+    <>
+      <div className="flex flex-col justify-between items-center gap-3 md:flex-row w-[90vw] m-auto py-4 fixed inset-[0_0_auto_0] z-[1] bg-gray-50">
+        <h1 className="text-teal-800 text-3xl font-bold">Shuffler</h1>
+        <div className="flex flex-col justify-between items-end gap-3 md:flex-row md:w-[50rem]">
+          <label className="flex flex-col gap-1 w-full">
+            {/* <span className="text-teal-600 text-sm font-bold">YouTube</span> */}
+            <div className="relative">
+              <span className="flex justify-center items-center w-12 h-11 text-white bg-[#DA1725] pl-1 absolute top-[2px] left-[2px] rounded-[6rem_0_0_6rem]">
+                <img src={ICON_YOUTUBE} className="size-6" />
+              </span>
+              <input
+                name="youtube"
+                className="w-full h-12 text-gray-900 bg-white border-2 border-gray-300 pl-14 pr-3 rounded-full outline-offset-4 focus-visible:outline-teal-600"
+                placeholder="https://youtube.com/playlist?list=xxx"
+                value={youtubeUrl}
+                onChange={(event) => setYoutubeUrl(event.target.value)}
               />
-            </span>
-            <span className="text-sm">{playing.track.artist}</span>
-          </div>
+            </div>
+          </label>
+          <label className="flex flex-col gap-1 w-full">
+            {/* <span className="text-teal-600 text-sm font-bold">Spotify</span> */}
+            <div className="relative">
+              <span className="flex justify-center items-center w-12 h-11 bg-[#2ebd59] pl-1 absolute top-[2px] left-[2px] rounded-[6rem_0_0_6rem]">
+                <img src={ICON_SPOTIFY} className="size-6" />
+              </span>
+              <input
+                name="spotify"
+                className="w-full h-12 text-gray-900 bg-white border-2 border-gray-300 pl-14 pr-3 rounded-full outline-offset-4 focus-visible:outline-teal-600"
+                placeholder="https://open.spotify.com/playlist/xxx"
+                value={spotifyUrl}
+                onChange={(event) => setSpotifyUrl(event.target.value)}
+              />
+            </div>
+          </label>
+          <button
+            className="flex-none w-20 h-12 text-white px-3 rounded-md font-bold outline-offset-[3px] transition-colors enabled:bg-teal-500 enabled:hover:bg-teal-600 enabled:active:bg-teal-700 focus-visible:outline-teal-600 disabled:bg-gray-300"
+            disabled={!youtubeUrl.length || !spotifyUrl.length}
+            onClick={() => getYoutube()}
+          >
+            検索
+          </button>
         </div>
       </div>
-      <button
-        className="flex-none w-20 h-12 text-white px-3 rounded-md font-bold outline-offset-[3px] transition-colors enabled:bg-teal-500 enabled:hover:bg-teal-600 enabled:active:bg-teal-700 focus-visible:outline-teal-600 disabled:bg-gray-300"
-        onClick={() => setPlaylist((prev) => shuffle(prev))}
-      >
-        シャッフル
-      </button>
-      <ul className="flex flex-col items-stretch gap-3 w-full">
-        {playlist.map(({ track }) => (
-          <li key={track.id} className="flex gap-2 w-full">
-            <div
-              className={cvaTrackThumbnail({
-                playing: track.id === playing.track.id,
-              })}
+      <main className="flex flex-col items-stretch gap-6 w-[95vw] max-w-[60rem] m-auto pt-24">
+        {error && (
+          <p>
+            プレイリストがヒットしませんでした。URLを確認し、もう一度お試しください。
+          </p>
+        )}
+        <div className="flex justify-between gap-10 flex-col md:flex-row">
+          <div className="flex flex-col items-start gap-6 flex-none size-fit max-w-[32rem]">
+            <div className="flex justify-center items-center size-full max-w-[32rem] max-h-[32rem] aspect-square overflow-hidden relative rounded-2xl shadow-2xl">
+              {playlist[currentIndex].type === "youtube" ? (
+                <>
+                  <div className="flex justify-center items-center h-full aspect-video overflow-hidden absolute">
+                    <div className="flex justify-center items-center aspect-video blur-md scale-105">
+                      <img src={playlist[currentIndex].track.thumbnail.url} />
+                    </div>
+                  </div>
+                  <div className="flex items-center aspect-video overflow-hidden relative z-auto">
+                    <img src={playlist[currentIndex].track.thumbnail.url} />
+                  </div>
+                </>
+              ) : (
+                <img src={playlist[currentIndex].track.thumbnail.url} />
+              )}
+            </div>
+            <div className="flex flex-col items-stretch gap-2 w-full">
+              <div className="flex items-center gap-1.5 text-gray-700">
+                <span
+                  className={cvaCurrentType({
+                    type: playlist[currentIndex].type,
+                  })}
+                >
+                  <img
+                    src={
+                      currentIconMap[playlist[currentIndex].type ?? "youtube"]
+                    }
+                    className="size-5"
+                  />
+                </span>
+                <span className="text-sm">
+                  {playlist[currentIndex].track.artist}
+                </span>
+              </div>
+              <div className="text-gray-900 text-2xl font-bold leading-9">
+                {playlist[currentIndex].track.title}
+              </div>
+            </div>
+            <div className="flex justify-between w-full">
+              <button onClick={() => setCurrentIndex((prev) => prev - 1)}>
+                Prev
+              </button>
+              <button onClick={() => setCurrentIndex((prev) => prev + 1)}>
+                Next
+              </button>
+            </div>
+            <button
+              className="flex-none h-12 text-white px-3 rounded-md font-bold outline-offset-[3px] transition-colors enabled:bg-teal-500 enabled:hover:bg-teal-600 enabled:active:bg-teal-700 focus-visible:outline-teal-600 disabled:bg-gray-300"
+              onClick={() => {
+                setPlaylist(shuffle(playlist, currentIndex));
+                setCurrentIndex(0);
+              }}
             >
-              <img src={track.thumbnail.url} className="size-full" />
-            </div>
-            <div className="flex items-stretch flex-col gap-0.5 w-[calc(100%_-_2.25rem_-_0.5rem)]">
-              <div
-                className={cvaTrackTitle({
-                  playing: track.id === playing.track.id,
-                })}
-              >
-                {track.title}
-              </div>
-              <div
-                className={cvaTrackArtistName({
-                  playing: track.id === playing.track.id,
-                })}
-              >
-                {track.artist}
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </main>
+              シャッフル
+            </button>
+          </div>
+          <ul className="flex flex-col items-stretch gap-1.5 flex-grow">
+            {playlist.map(({ track }, index) => (
+              <li key={track.id + index}>
+                <button
+                  className={cvaTrack({
+                    current: track.id === playlist[currentIndex].track.id,
+                  })}
+                  onClick={() => setCurrentIndex(index)}
+                  disabled={currentIndex === index}
+                >
+                  <div
+                    className={cvaTrackThumbnail({
+                      current: track.id === playlist[currentIndex].track.id,
+                    })}
+                  >
+                    <img src={track.thumbnail.url} className="size-full" />
+                  </div>
+                  <div className="flex items-stretch flex-col w-[calc(100%_-_2.25rem_-_0.5rem)]">
+                    <div
+                      className={cvaTrackTitle({
+                        current: track.id === playlist[currentIndex].track.id,
+                      })}
+                    >
+                      {track.title}
+                    </div>
+                    <div
+                      className={cvaTrackArtistName({
+                        current: track.id === playlist[currentIndex].track.id,
+                      })}
+                    >
+                      {track.artist}
+                    </div>
+                  </div>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+    </>
   );
 }
