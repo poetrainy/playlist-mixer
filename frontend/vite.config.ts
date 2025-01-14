b88e37761aa34d157aa076a119e96bfd08cc0eb8
poetrainy <manamisato2001@gmail.com>
2025-01-14 22:01:53 +0900

    remove: index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
-import { resolve } from "node:path";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
