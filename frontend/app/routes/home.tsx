2b618f084a543aa66fcb2fb6863aff1e99e430d4
poetrainy <manamisato2001@gmail.com>
2025-01-14 21:36:35 +0900

    feat: レスポンシブデザイン

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,7 +1,13 @@
 import type { Route } from "./+types/home";
 import { useState } from "react";
-import ICON_SPOTIFY from "~/icons/icon_spotify.png";
-import ICON_YOUTUBE from "~/icons/icon_youtube.svg";
+import ICON_SPOTIFY from "~/icons/icon-spotify.svg";
+import ICON_YOUTUBE from "~/icons/icon-youtube.svg";
+import ICON_PLAY from "~/icons/icon-play.svg";
+import ICON_PREV from "~/icons/icon-prev.svg";
+import ICON_NEXT from "~/icons/icon-next.svg";
+import ICON_REPEAT from "~/icons/icon-repeat.svg";
+import ICON_SHUFFLE from "~/icons/icon-shuffle.svg";
+import ICON_HEADPHONE from "~/icons/icon-headphone.svg";
 import { getSpotifyPlaylist, getYoutubePlaylist } from "~/api/get";
 import type { PlaylistType, TrackType } from "~/types/common";
 import { cva } from "class-variance-authority";
@@ -12,6 +18,46 @@ const currentIconMap = {
   youtube: ICON_YOUTUBE,
   spotify: ICON_SPOTIFY,
 } as const;
+
+const cvaControlButton = cva(
+  [
+    "flex",
+    "items-center",
+    "justify-center",
+    "rounded-lg",
+
+    "focus-visible:outline",
+    "focus-visible:outline-teal-600",
+    "focus-visible:outline-2",
+    "focus-visible:outline-offset-[3px]",
+  ],
+  {
+    variants: {
+      variant: {
+        fill: [
+          "enabled:bg-teal-500",
+          "enabled:hover:bg-teal-600",
+          "enabled:active:bg-teal-700",
+          "disabled:bg-gray-300",
+        ],
+        ghost: [
+          "bg-transparent",
+          "enabled:hover:bg-gray-200",
+          "enabled:active:bg-gray-300",
+        ],
+      },
+      type: {
+        prev: ["size-12", "md:size-14", "pr-0.5"],
+        next: ["size-12", "md:size-14", "pl-0.5"],
+        play: ["size-14", "md:size-20"],
+        option: ["w-full", "h-8", "md:h-10"],
+      },
+    },
+    defaultVariants: {
+      variant: "ghost",
+    },
+  }
+);
 
 const cvaTrack = cva(
   [
@@ -108,7 +154,7 @@ const cvaCurrentType = cva(
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Shuffle YouTube & Spotify Playlists" },
+    { title: "Music Shuffler" },
     { name: "description", content: "Shuffle YouTube & Spotify Playlists" },
   ];
 }
@@ -129,6 +175,7 @@ export default function Home() {
     ...spotifyPlaylist.map((track) => ({ type: "spotify" as const, track })),
   ]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [repeat, setRepeat] = useState(false);
 
   const search = async () => {
     setError(false);
@@ -162,9 +209,12 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col justify-between items-center gap-3 md:flex-row w-[90vw] m-auto py-4 fixed inset-[0_0_auto_0] z-[1] bg-gray-50">
-        <h1 className="text-teal-800 text-3xl font-bold">Shuffler</h1>
-        <div className="flex flex-col justify-between items-end gap-3 md:flex-row md:w-[50rem]">
+      <div className="flex flex-col justify-between items-center gap-3 md:flex-row w-[95vw] md:w-[90vw] m-auto py-4 md:fixed md:inset-[0_0_auto_0] z-[1] bg-gray-50">
+        <h1 className="flex items-center gap-2 text-teal-800 text-3xl font-bold">
+          <img src={ICON_HEADPHONE} className="size-7" />
+          <span className="hidden md:block">Music Shuffler</span>
+        </h1>
+        <div className="flex justify-between items-end gap-3 md:w-[50rem]">
           <label className="flex flex-col gap-1 w-full">
             <div className="relative">
               <span className="flex justify-center items-center w-12 h-11 text-white bg-[#DA1725] pl-1 absolute top-[2px] left-[2px] rounded-[6rem_0_0_6rem]">
@@ -193,27 +243,26 @@ export default function Home() {
               />
             </div>
           </label>
-          <button
+          {/* <button
             className="flex-none w-20 h-12 text-white px-3 rounded-md font-bold outline-offset-[3px] transition-colors enabled:bg-teal-500 enabled:hover:bg-teal-600 enabled:active:bg-teal-700 focus-visible:outline-teal-600 disabled:bg-gray-300"
             disabled
             // disabled={!youtubeUrl.length || !spotifyUrl.length}
             // onClick={() => search()}
           >
-            検索
-          </button>
+            Shuffle!
+          </button> */}
         </div>
       </div>
-      <main className="flex justify-between items-stretch flex-col md:flex-row gap-10 w-[95vw] max-w-[60rem] m-auto">
-        <div className="flex flex-col items-start gap-6 flex-none size-fit max-w-[32rem] pt-24 sticky top-0">
+      <main className="flex justify-center md:justify-between items-stretch flex-col md:flex-row gap-10 w-[95vw] max-w-[60rem] m-auto">
+        <div className="flex flex-col items-start gap-6 flex-none size-fit max-w-[32rem] mx-auto md:m-0 md:pt-24 md:sticky top-0">
           <div className="flex justify-center items-center size-full max-w-[32rem] max-h-[32rem] aspect-square overflow-hidden relative rounded-2xl shadow-2xl">
             {playlist[currentIndex].type === "youtube" ? (
               <>
-                <div className="flex justify-center items-center h-full aspect-video overflow-hidden absolute">
-                  <div className="flex justify-center items-center aspect-video blur-md scale-105">
-                    <img src={playlist[currentIndex].track.thumbnail.url} />
-                  </div>
-                </div>
-                <div className="flex items-center aspect-video overflow-hidden relative z-auto">
+                <img
+                  src={playlist[currentIndex].track.thumbnail.url}
+                  className="size-full scale-150 blur-sm absolute"
+                />
+                <div className="flex items-center aspect-video overflow-hidden relative z-auto shadow-2xl">
                   <img src={playlist[currentIndex].track.thumbnail.url} />
                 </div>
               </>
@@ -221,7 +270,7 @@ export default function Home() {
               <img src={playlist[currentIndex].track.thumbnail.url} />
             )}
           </div>
-          <div className="flex flex-col items-stretch gap-2 w-full">
+          <div className="flex flex-col items-stretch gap-2 flex-grow w-full">
             <div className="flex items-center gap-1.5 text-gray-700">
               <span
                 className={cvaCurrentType({
@@ -241,25 +290,49 @@ export default function Home() {
               {playlist[currentIndex].track.title}
             </div>
           </div>
-          <div className="flex justify-between w-full">
-            <button onClick={() => setCurrentIndex((prev) => prev - 1)}>
-              Prev
-            </button>
-            <button onClick={() => setCurrentIndex((prev) => prev + 1)}>
-              Next
-            </button>
+          <div className="w-full h-3 bg-gray-200 rounded-full relative after:block after:w-[70%] after:h-full after:bg-teal-500 after:absolute after:rounded-full" />
+          <div className="flex items-center justify-between flex-col gap-1 w-full px-10 md:px-12">
+            <div className="flex items-center justify-between w-full">
+              <button
+                className={cvaControlButton({ type: "prev" })}
+                aria-label="前の曲へ"
+                onClick={() => setCurrentIndex((prev) => prev - 1)}
+              >
+                <img src={ICON_PREV} className="size-8" />
+              </button>
+              <button className={cvaControlButton({ type: "play" })}>
+                <img src={ICON_PLAY} className="size-10 md:size-12" />
+              </button>
+              <button
+                className={cvaControlButton({ type: "next" })}
+                aria-label="次の曲へ"
+                onClick={() => setCurrentIndex((prev) => prev + 1)}
+              >
+                <img src={ICON_NEXT} className="size-8" />
+              </button>
+            </div>
+            <div className="flex gap-2 w-full">
+              <button
+                className={cvaControlButton({ type: "option" })}
+                aria-label="リピート"
+                onClick={() => setRepeat(!repeat)}
+              >
+                <img src={ICON_REPEAT} className="size-5" />
+              </button>
+              <button
+                className={cvaControlButton({ type: "option" })}
+                aria-label="シャッフル"
+                onClick={() => {
+                  setPlaylist(shuffle(playlist, currentIndex));
+                  setCurrentIndex(0);
+                }}
+              >
+                <img src={ICON_SHUFFLE} className="size-5" />
+              </button>
+            </div>
           </div>
-          <button
-            className="flex-none h-12 text-white px-3 rounded-md font-bold outline-offset-[3px] transition-colors enabled:bg-teal-500 enabled:hover:bg-teal-600 enabled:active:bg-teal-700 focus-visible:outline-teal-600 disabled:bg-gray-300"
-            onClick={() => {
-              setPlaylist(shuffle(playlist, currentIndex));
-              setCurrentIndex(0);
-            }}
-          >
-            シャッフル
-          </button>
         </div>
-        <ul className="flex flex-col items-stretch gap-1.5 flex-grow pt-24 pb-8">
+        <ul className="flex flex-col items-stretch gap-1.5 flex-grow md:pt-24 pb-8">
           {playlist.map(({ track }, index) => (
             <li key={track.id + index}>
               <button
