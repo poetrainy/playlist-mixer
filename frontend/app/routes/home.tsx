c0203c7e6acd6d3799d3f793758ca5a7920a8373
poetrainy <manamisato2001@gmail.com>
2025-01-16 22:11:10 +0900

    fix: updated appearance

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,10 +1,9 @@
 import type { Route } from "./+types/home";
-import { cva } from "class-variance-authority";
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import Logo from "~/components/Logo";
-import ICON_SPOTIFY_WHITE from "~/icons/icon-spotify-white.svg";
-import ICON_YOUTUBE_WHITE from "~/icons/icon-youtube-white.svg";
+import ICON_SPOTIFY_BLAND from "~/icons/icon-spotify-bland.svg";
+import ICON_YOUTUBE_BLAND from "~/icons/icon-youtube-bland.svg";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -35,38 +34,41 @@ export default function Home() {
     }
   };
 
+  const searchServices = [
+    {
+      name: "youtube",
+      icon: ICON_YOUTUBE_BLAND,
+      placeholder: "https://youtube.com/playlist?list=xxx",
+      value: youtubeUrl,
+      onChange: (event: React.ChangeEvent<HTMLInputElement>) =>
+        setYoutubeUrl(event.target.value),
+    },
+    {
+      name: "spotify",
+      icon: ICON_SPOTIFY_BLAND,
+      placeholder: "https://open.spotify.com/playlist/xxx",
+      value: spotifyUrl,
+      onChange: (event: React.ChangeEvent<HTMLInputElement>) =>
+        setSpotifyUrl(event.target.value),
+    },
+  ];
+
   return (
     <main className="flex flex-col items-center gap-8 py-40 px-[2.5vw]">
       <Logo homePage />
       <div className="flex flex-col justify-between items-end gap-2.5 w-full md:max-w-2xl">
-        <label className="flex flex-col gap-1 w-full">
-          <div className="relative">
-            <span className="flex justify-center items-center w-12 h-11 text-white bg-[#DA1725] pl-1 absolute top-[2px] left-[2px] rounded-[6rem_0_0_6rem]">
-              <img src={ICON_YOUTUBE_WHITE} className="size-6" />
-            </span>
+        {searchServices.map(({ name, icon, placeholder, value, onChange }) => (
+          <label key={name} className="flex items-center gap-1 w-full relative">
+            <img src={icon} className="flex-none size-6 absolute left-4" />
             <input
-              name="youtube"
-              className="w-full h-12 text-gray-900 bg-white border-2 border-gray-300 pl-14 pr-3 rounded-full outline-offset-4 focus-visible:outline-teal-600"
-              placeholder="https://youtube.com/playlist?list=xxx"
-              value={youtubeUrl}
-              onChange={(event) => setYoutubeUrl(event.target.value)}
+              name={name}
+              className="w-full h-12 text-gray-900 bg-white border-2 border-gray-300 pl-12 pr-4 rounded-full outline-offset-4 focus-visible:outline-teal-600"
+              placeholder={placeholder}
+              value={value}
+              onChange={(event) => onChange(event)}
             />
-          </div>
-        </label>
-        <label className="flex flex-col gap-1 w-full">
-          <div className="relative">
-            <span className="flex justify-center items-center w-12 h-11 bg-[#2ebd59] pl-1 absolute top-[2px] left-[2px] rounded-[6rem_0_0_6rem]">
-              <img src={ICON_SPOTIFY_WHITE} className="size-6" />
-            </span>
-            <input
-              name="spotify"
-              className="w-full h-12 text-gray-900 bg-white border-2 border-gray-300 pl-14 pr-3 rounded-full outline-offset-4 focus-visible:outline-teal-600"
-              placeholder="https://open.spotify.com/playlist/xxx"
-              value={spotifyUrl}
-              onChange={(event) => setSpotifyUrl(event.target.value)}
-            />
-          </div>
-        </label>
+          </label>
+        ))}
       </div>
       <button
         className="flex items-center justify-center flex-none w-20 h-12 text-white px-3 rounded-md font-bold outline-offset-[3px] transition-colors focus-visible:outline-teal-600 enabled:bg-teal-500 enabled:hover:bg-teal-600 enabled:active:bg-teal-700 disabled:bg-gray-300"
